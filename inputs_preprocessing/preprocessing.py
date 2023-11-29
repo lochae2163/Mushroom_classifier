37e389c30655a229a7d3a600a4e8bb4704fc3209
lochae2163 <lochanpoon11@gmail.com>
2023-11-29 11:34:29 +0900

    Creating inputs_preprocessing folder and adding preprocessing.py file

---
+++ inputs_preprocessing/preprocessing.py
@@ -0,0 +1,44 @@
+class Preprocessor:
+    def __init__(self):
+        self.features = [
+            'cap-shape', 'cap-surface', 'cap-color', 'bruises', 'odor', 'gill-attachment',
+            'gill-spacing', 'gill-size', 'gill-color', 'stalk-shape', 'stalk-root',
+            'stalk-surface-above-ring', 'stalk-surface-below-ring', 'stalk-color-above-ring',
+            'stalk-color-below-ring', 'veil-color', 'ring-number', 'ring-type', 'spore-print-color',
+            'population', 'habitat'
+        ]
+        self.categories = {
+            'cap-shape': ['b', 'c', 'x', 'f', 'k', 's'],
+            'cap-surface': ['f', 'g', 'y', 's'],
+            'cap-color': ['n', 'b', 'c', 'g', 'r', 'p', 'u', 'e', 'w', 'y'],
+            'bruises': ['t', 'f'],
+            'odor': ['a', 'l', 'c', 'y', 'f', 'm', 'n', 'p', 's'],
+            'gill-attachment': ['a', 'd', 'f', 'n'],
+            'gill-spacing': ['c', 'w', 'd'],
+            'gill-size': ['b', 'n'],
+            'gill-color': ['k', 'n', 'b', 'h', 'g', 'r', 'o', 'p', 'u', 'e', 'w', 'y'],
+            'stalk-shape': ['e', 't'],
+            'stalk-root': ['b', 'c', 'u', 'e', 'z', 'r', '?'],
+            'stalk-surface-above-ring': ['f', 'y', 'k', 's'],
+            'stalk-surface-below-ring': ['f', 'y', 'k', 's'],
+            'stalk-color-above-ring': ['n', 'b', 'c', 'g', 'o', 'p', 'e', 'w', 'y'],
+            'stalk-color-below-ring': ['n', 'b', 'c', 'g', 'o', 'p', 'e', 'w', 'y'],
+            'veil-color': ['n', 'o', 'w', 'y'],
+            'ring-number': ['n', 'o', 't'],
+            'ring-type': ['c', 'e', 'f', 'l', 'n', 'p', 's', 'z'],
+            'spore-print-color': ['k', 'n', 'b', 'h', 'r', 'o', 'u', 'w', 'y'],
+            'population': ['a', 'c', 'n', 's', 'v', 'y'],
+            'habitat': ['g', 'l', 'm', 'p', 'u', 'w', 'd']
+        }
+        self.processed_inputs = []* 94
+
+    def _one_hot_encode(self, user_inputs):
+        processed_inputs = [0] * 94  # Ensure consistent size for all inputs
+
+        for feature, value in zip(self.features, user_inputs):
+            if value in self.categories[feature]:
+                index = list(self.categories.keys()).index(feature) * len(self.categories[feature])
+                category_index = self.categories[feature].index(value)
+                processed_inputs[index + category_index] = 1
+
+        return processed_inputs
